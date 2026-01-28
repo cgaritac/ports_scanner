94b93be41c65253149bc258c8f8ec8c05ea55bf5
Carlos Garita Campos <cgaritac@gmail.com>
2026-01-27 23:07:51 -0600

    Feat: Update logic

--- port_scanner.py
+++ port_scanner.py
@@ -1,16 +1,17 @@
 import socket
 
+def scan_port(ip):
+    for port in range(1, 65535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print(f"Port {port} is open")
+            sock.close()
+        else:
+            print(f"Port {port} is closed")
+            sock.close()
+
 ip = input("Enter the IP address to scan: ")
 
-for port in range(1, 65535):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-
-    result = sock.connect_ex((ip, port))
-
-    if result == 0:
-        print(f"Port {port} is open")
-        sock.close()
-    else:
-        print(f"Port {port} is closed")
-        sock.close()
+scan_port(ip)
